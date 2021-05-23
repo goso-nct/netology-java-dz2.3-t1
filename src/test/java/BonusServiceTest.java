17d58a73a629f3ed5a823df815c072365f96773d
Golubev Vladimir <goso@list.ru>
2021-05-23 23:13:22 +0300

    add tests Unregistered, Zero, Negative

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -23,16 +23,51 @@ class BonusServiceTest {
     @Test
     void shouldCalculateForRegisteredAndOverLimit() {
         BonusService service = new BonusService();
-
-        // подготавливаем данные:
         long amount = 1_000_000_60;
         boolean registered = true;
         long expected = 500;
-
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+    @Test
+    void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateZeroForZeroAmount() {
+        BonusService service = new BonusService();
+        long amount = 0;
+        boolean registered = false;
+        long expected = 0;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateZeroForNegativeAmount() {
+        // ЯТД
+        BonusService service = new BonusService();
+        long amount = -1_000_000_60;
+        boolean registered = false;
+        long expected = 0;
+        long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
     }
 }
